fb0699131487d8101ea9f274cff4c65681d982b9
dsshap <dss.shapiro@gmail.com>
2023-01-30 17:43:27 -0600

    adding argSort

--- src/sorting/BubbleSort.sol
+++ src/sorting/BubbleSort.sol
@@ -23,6 +23,26 @@ library BubbleSort {
     }
 
     /**
+     * @dev sort an uint256 array in place with bubble sort and return indexes
+     */
+    function argSort(uint256[] memory arr) internal pure returns (uint256[] memory idxs) {
+        idxs = new uint256[](arr.length);
+        unchecked {
+            uint256 i;
+            for (i; i < arr.length; ++i) idxs[i] = i;
+            if (arr.length <= 1) return(idxs);
+            for (i = 0; i < arr.length; ++i) {
+                for (uint256 j = i + 1; j < arr.length; ++j) {
+                    if (arr[i] > arr[j]) {
+                        (arr[i], arr[j]) = (arr[j], arr[i]);
+                        (idxs[i], idxs[j]) = (idxs[j], idxs[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
      * @dev sort an int256 array in place with bubble sort
      */
     function sort(int256[] memory arr) internal pure {
@@ -32,6 +52,26 @@ library BubbleSort {
                 for (uint256 j = i + 1; j < arr.length; ++j) {
                     if (arr[i] > arr[j]) {
                         (arr[i], arr[j]) = (arr[j], arr[i]);
+                    }
+                }
+            }
+        }
+    }
+
+    /**
+     * @dev sort an int256 array in place with bubble sort and return indexes
+     */
+    function argSort(int256[] memory arr) internal pure returns (uint256[] memory idxs) {
+        idxs = new uint256[](arr.length);
+        unchecked {
+            uint256 i;
+            for (i; i < arr.length; ++i) idxs[i] = i;
+            if (arr.length <= 1) return(idxs);
+            for (i = 0; i < arr.length; ++i) {
+                for (uint256 j = i + 1; j < arr.length; ++j) {
+                    if (arr[i] > arr[j]) {
+                        (arr[i], arr[j]) = (arr[j], arr[i]);
+                        (idxs[i], idxs[j]) = (idxs[j], idxs[i]);
                     }
                 }
             }
--- test/coverage/sorting/BubbleSort.t.sol
+++ test/coverage/sorting/BubbleSort.t.sol
@@ -11,9 +11,19 @@ contract BubbleSortTester {
         return x;
     }
 
+    function argSort(int256[] memory x) external pure returns (int256[] memory, uint256[] memory idxs) {
+        idxs = BubbleSort.argSort(x);
+        return (x, idxs);
+    }
+
     function sort(uint256[] memory x) external pure returns (uint256[] memory) {
         BubbleSort.sort(x);
         return x;
+    }
+
+    function argSort(uint256[] memory x) external pure returns (uint256[] memory, uint256[] memory idxs) {
+        idxs = BubbleSort.argSort(x);
+        return (x, idxs);
     }
 }
 
@@ -39,6 +49,16 @@ contract BubbleSortLibCoverage is Test {
         return arr;
     }
 
+    function _getDefaultIntDupArray() internal pure returns (int256[] memory) {
+        int256[] memory arr = new int256[](5);
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 1;
+        arr[3] = 1;
+        arr[4] = 1;
+        return arr;
+    }
+
     /**
      * util for test data
      */
@@ -49,6 +69,16 @@ contract BubbleSortLibCoverage is Test {
         arr[2] = 2;
         arr[3] = 4;
         arr[4] = 3;
+        return arr;
+    }
+
+    function _getDefaultUintDupArray() internal pure returns (uint256[] memory) {
+        uint256[] memory arr = new uint256[](5);
+        arr[0] = 1;
+        arr[1] = 1;
+        arr[2] = 1;
+        arr[3] = 1;
+        arr[4] = 1;
         return arr;
     }
 
@@ -70,11 +100,65 @@ contract BubbleSortLibCoverage is Test {
         assertEq(arr[4], 5);
     }
 
+    function testArgSortIntLength0() public {
+        uint256[] memory idxs;
+        int256[] memory arr = new int256[](1);
+        arr[0] = 50;
+        (arr, idxs) = tester.argSort(arr);
+        assertEq(arr[0], 50);
+        assertEq(idxs[0], 0);
+    }
+
+    function testArgSortInt() public {
+        uint256[] memory idxs;
+        int256[] memory arr = _getDefaultIntArray();
+        (arr, idxs) = tester.argSort(arr);
+
+        assertEq(arr[0], -3);
+        assertEq(arr[1], -2);
+        assertEq(arr[2], 1);
+        assertEq(arr[3], 4);
+        assertEq(arr[4], 5);
+
+        assertEq(idxs[0], 4);
+        assertEq(idxs[1], 2);
+        assertEq(idxs[2], 0);
+        assertEq(idxs[3], 3);
+        assertEq(idxs[4], 1);
+    }
+
+    function testArgSortIntsDup() public {
+        uint256[] memory idxs;
+        int256[] memory arr = _getDefaultIntDupArray();
+        (arr, idxs) = tester.argSort(arr);
+
+        assertEq(arr[0], 1);
+        assertEq(arr[1], 1);
+        assertEq(arr[2], 1);
+        assertEq(arr[3], 1);
+        assertEq(arr[4], 1);
+
+        assertEq(idxs[0], 0);
+        assertEq(idxs[1], 1);
+        assertEq(idxs[2], 2);
+        assertEq(idxs[3], 3);
+        assertEq(idxs[4], 4);
+    }
+
     function testSortUintLength0() public {
         uint256[] memory arr = new uint256[](1);
         arr[0] = 50;
         arr = tester.sort(arr);
         assertEq(arr[0], 50);
+    }
+
+    function testArgSortUintLength0() public {
+        uint256[] memory idxs;
+        uint256[] memory arr = new uint256[](1);
+        arr[0] = 50;
+        (arr, idxs) = tester.argSort(arr);
+        assertEq(arr[0], 50);
+        assertEq(idxs[0], 0);
     }
 
     function testSortUint() public {
@@ -86,5 +170,41 @@ contract BubbleSortLibCoverage is Test {
         assertEq(arr[2], 3);
         assertEq(arr[3], 4);
         assertEq(arr[4], 5);
+    }
+
+    function testArgSortUint() public {
+        uint256[] memory idxs;
+        uint256[] memory arr = _getDefaultUintArray();
+        (arr, idxs) = tester.argSort(arr);
+
+        assertEq(arr[0], 1);
+        assertEq(arr[1], 2);
+        assertEq(arr[2], 3);
+        assertEq(arr[3], 4);
+        assertEq(arr[4], 5);
+
+        assertEq(idxs[0], 0);
+        assertEq(idxs[1], 2);
+        assertEq(idxs[2], 4);
+        assertEq(idxs[3], 3);
+        assertEq(idxs[4], 1);
+    }
+
+    function testArgSortUintDup() public {
+        uint256[] memory idxs;
+        uint256[] memory arr = _getDefaultUintDupArray();
+        (arr, idxs) = tester.argSort(arr);
+
+        assertEq(arr[0], 1);
+        assertEq(arr[1], 1);
+        assertEq(arr[2], 1);
+        assertEq(arr[3], 1);
+        assertEq(arr[4], 1);
+
+        assertEq(idxs[0], 0);
+        assertEq(idxs[1], 1);
+        assertEq(idxs[2], 2);
+        assertEq(idxs[3], 3);
+        assertEq(idxs[4], 4);
     }
 }
